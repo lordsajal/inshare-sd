5f2ae1c4cb7f6f54784faa17291ec3dd05327419
swajal das <42321549+swajal-github@users.noreply.github.com>
2021-05-01 18:08:08 +0530

    Update download.js

--- routes/download.js
+++ routes/download.js
@@ -2,11 +2,7 @@ const router = require("express").Router();
 const File = require("../models/file");
 
 router.get("/:uuid", async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*")
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+
   const file = await File.findOne({ uuid: req.params.uuid });
   if (!file) {
     return res.render("download", { error: "lisk has been expired." });
